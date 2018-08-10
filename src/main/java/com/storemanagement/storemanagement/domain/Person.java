f0af387dc79fcd1dce511259758321967a6c87e5
LuisMario01 <luismario.depaz@gmail.com>
2018-08-10 16:08:11 -0600

    Person domain class

---
+++ src/main/java/com/storemanagement/storemanagement/domain/Person.java
@@ -0,0 +1,29 @@
+package com.storemanagement.storemanagement.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="person")
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idPerson;
+	
+	@NotNull
+	@Column(name="username")
+	private String username;
+	
+	@NotNull
+	@Column(name="password")
+	private String password;
+	
+	@NotNull
+	@Column(name="role")
+	private Integer role;
+}
